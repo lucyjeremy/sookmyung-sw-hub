c9e708b7ccd3f5f11deb9cad3d979d767ae0f2e5
lucyjeremy <lucyjeremy0209@gmail.com>
2026-05-26 11:22:36 +0900

    React Vite Template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-purple-50 flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-purple-700">
+        숙명 SW 비교과 Hub
+      </h1>
+    </div>
+  )
+}
+
+export default App
